26fdc9a40a99347f25ed1b04ab0f16da191feaed
mc-luc <mishashan32@gmail.com>
2018-04-09 14:56:53 -0400

    update

--- Client/Client.java
+++ Client/Client.java
@@ -13,7 +13,7 @@ class Client {
 
 		Client client = new Client();
 		System.out.println("connecting");
-		client.connect("www.hiscinema.com");
+		client.connect("abc/Video");
 		System.out.println("fin");
 
 		client.connect("www.hiscinema.com/Video");
@@ -21,13 +21,63 @@ class Client {
 
 	public void connect(String url) throws Exception{
 
+		boolean cached = false;
 		for(int i = 0; i < cache.length; i ++){
 			if(cache[i].name.equals(url)){
 				System.out.println("found, requesting");
+				cached = true;
+
+				//IGNORE THE INDEX.HTML
 				getRequest("index.html", cache[i].value.getHostString(), cache[i].value.getPort());
 			}
 		}
+
+		if(!cached){
+
+			DNSRecord temp = askDNS(url, localDNS.value.getHostString(), localDNS.value.getPort());
+
+
+
+			//getRequest("index.html", localDNS.value.getHostString(), localDNS.value.getPort());
+
+		}
+
 		// IF NOT ACCESS THE LOCAL DNS
+
+	}
+
+	//ASKS LOCAL DNS FOR DNS RECORD
+	public static DNSRecord askDNS(String url, String ip, int port) throws Exception{
+
+	  DatagramSocket clientSocket = new DatagramSocket();
+
+	  InetAddress IPAddress = InetAddress.getByName(ip);
+
+	  byte[] sendData = new byte[1024];
+    byte[] receiveData = new byte[1024];
+
+    String sentence = url;
+
+    sendData = sentence.getBytes();
+
+	  DatagramPacket sendPacket =
+			new DatagramPacket(sendData, sendData.length, IPAddress, port);
+      //new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+
+
+    clientSocket.send(sendPacket);
+
+    DatagramPacket receivePacket =
+      new DatagramPacket(receiveData, receiveData.length);
+
+    clientSocket.receive(receivePacket);
+
+    String returnedRecord = new String(receivePacket.getData());
+
+    System.out.println("FROM DNS:" + returnedRecord);
+    clientSocket.close();
+
+		return new DNSRecord("", new InetSocketAddress("localhost",0), "");
 
 	}
 
